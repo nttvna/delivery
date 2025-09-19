e36ea04bd8c2e3351b067b5201e7883d28deecc1
nt2vna <nt2vna@gmail.com>
2025-09-19 09:10:36 +0700

    update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,9 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -20,14 +20,28 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />
+          tabBarIcon: ({ color }) => <MaterialIcons size={30} name={'home'} color={color} />
+        }}
+      />
+       <Tabs.Screen
+        name="orders"
+        options={{
+          title: 'Orders',
+          tabBarIcon: ({ color }) => <MaterialIcons size={30} name={'article'} color={color} />,
+        }}
+      />
+       <Tabs.Screen
+        name="history"
+        options={{
+          title: 'History',
+          tabBarIcon: ({ color }) => <MaterialIcons size={30} name={'list'} color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.2.circle" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons size={30} name={'account-box'} color={color} />,
         }}
       />
     </Tabs>
